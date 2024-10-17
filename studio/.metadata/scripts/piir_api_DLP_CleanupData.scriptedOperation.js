dc706418006664ce35eb9a216876d91e9cbf239b
jfletcher <jfletcher@nuxeo.com>
2024-10-17 20:34:33 +0000

    Update Automation Scripting/piir_api_DLP_CleanupData

--- studio/.metadata/scripts/piir_api_DLP_CleanupData.scriptedOperation.js
+++ studio/.metadata/scripts/piir_api_DLP_CleanupData.scriptedOperation.js
@@ -1,3 +1,26 @@
 function run(input, params) {
+  
+  Console.log("DLP_CleanupData...");
+    
+  if(input.hasSchema("datalossprevention")) {
+    input = Document.ResetSchema(
+      input, {
+        'schema': "datalossprevention",
+        'save': true,
+        'saveDocument': true
+      });
+  }
+  
+  if(input.hasFacet("DataLossPrevention")) {
+    input = Document.RemoveFacet(
+      input, {
+        'facet': "DataLossPrevention",
+        'save': true
+      });
+  }
+  
+  Console.log("DLP_CleanupData.......DONE");
+  
+  return input;
 
 }
