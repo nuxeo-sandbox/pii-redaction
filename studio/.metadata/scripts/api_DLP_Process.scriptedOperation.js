c254910b620044a50516626f67401fbab9296fe3
Josh Fletcher <josh.fletcher@hyland.com>
2024-10-17 13:26:39 -0700

    Cleaup.

--- studio/.metadata/scripts/api_DLP_Process.scriptedOperation.js
+++ studio/.metadata/scripts/api_DLP_Process.scriptedOperation.js
@@ -1,75 +1,73 @@
-/*
-https://cloud.google.com/sensitive-data-protection/docs/high-sensitivity-infotypes-reference
-
-WARNING: Looking for all and every sensitive data is not a good practice
-         https://cloud.google.com/sensitive-data-protection/docs/concepts-infotypes
-  <<Avoid enabling infoType detectors that you don't need. Although the following are
-    useful in certain scenarios, these infoTypes can make requests run much more slowly
-    than requests that don't include them:>>
-*/
 function run(input, params) {
-  
-  Console.log("TestDLP...");
-  
-  Console.log("Env. google.dlp.infotypes => " + Env["google.dlp.infotypes"]);
 
+  logHelper("================================================================================")
+  logHelper("== Begin...");
+  logHelper("================================================================================")
+
+  logHelper("Env. google.dlp.infotypes => " + Env["google.dlp.infotypes"]);
+
+  // For a demo we check a lot of infotypes, but the docs recommend only
+  // checking the info types that you know you need, for performance reasons.
+  // https://cloud.google.com/sensitive-data-protection/docs/concepts-infotypes#higher-latency-detectors
+
+  // https://cloud.google.com/sensitive-data-protection/docs/high-sensitivity-infotypes-reference
   var infoTypesHighSensibility = "US_SOCIAL_SECURITY_NUMBER,US_PASSPORT,PASSPORT,US_DRIVERS_LICENSE_NUMBER,CREDIT_CARD_NUMBER,IBAN_CODE,SWIFT_CODE,US_EMPLOYER_IDENTIFICATION_NUMBER,US_INDIVIDUAL_TAXPAYER_IDENTIFICATION_NUMBER";
-  
-  var lessImportantTypes = "GENERIC_ID,STREET_ADDRESS,DATE_OF_BIRTH,PERSON_NAME,GENDER,LAST_NAME,FIRST_NAME,EMAIL_ADDRESS";
-  
-  var srcBlob = input["file:content"];
-  
-  var mimetype = srcBlob.mimeType;
-  
-  var blobs = [];
-  
-  if (mimetype === "application/pdf") {
-    
-    Context.SetMetadataFromBlob(srcBlob, {});
-    Console.log(ctx.binaryMetadata);
-    
-    var pageCount = ctx.binaryMetadata.PageCount;
-    
-    Console.log(pageCount);
 
-    for(var i=1; i<= pageCount;i++) {
+  var lessImportantTypes = "GENERIC_ID,STREET_ADDRESS,DATE_OF_BIRTH,PERSON_NAME,GENDER,LAST_NAME,FIRST_NAME,EMAIL_ADDRESS";
+
+  var srcBlob = input["file:content"];
+
+  var mimetype = srcBlob.mimeType;
+
+  var blobs = [];
+
+  if (mimetype === "application/pdf") {
+
+    Context.SetMetadataFromBlob(srcBlob, {});
+    logHelper(ctx.binaryMetadata);
+
+    var pageCount = ctx.binaryMetadata.PageCount;
+
+    logHelper(pageCount);
+
+    for (var i = 1; i <= pageCount; i++) {
       blobs.push(PDF.ExtractPages(input, {
-        startPage : ""+i,
-        endPage: ""+i
+        startPage: "" + i,
+        endPage: "" + i
       }));
     }
-    
-    Console.log(blobs);
-    
-    blobs = toJsArray(blobs).map(function(blob) {
-      Console.log(blob);
+
+    logHelper(blobs);
+
+    blobs = toJsArray(blobs).map(function (blob) {
+      logHelper(blob);
       return Blob.RunConverter(blob, {
         'converter': 'simpleJpegCompressor'
       });
     });
-    Console.log(blobs);
+    logHelper(blobs);
   } else {
     blobs.push(srcBlob);
   }
-  
+
   var findings = [];
-  
-  blobs.forEach(function(blob, index) {
+
+  blobs.forEach(function (blob, index) {
 
     try {
       //get findings
       var resultBlob = Blob.IdentifySensitiveData(blob, {
         'infotypes': infoTypesHighSensibility + "," + lessImportantTypes, // let's try all of them
         'maxfindings': 20,
-        'convertToText':'false'
+        'convertToText': 'false'
       });
 
       //collect findings
-      Console.log(resultBlob.getString());
+      logHelper(resultBlob.getString());
 
       var pageFindings = JSON.parse(resultBlob.getString());
 
-      pageFindings.findings.forEach(function(finding) {
+      pageFindings.findings.forEach(function (finding) {
         var location = JSON.parse(finding.locationJsonStr);
         delete finding.locationJsonStr;
         location.pageNumber = index;
@@ -77,8 +75,8 @@ function run(input, params) {
         findings.push(finding);
       });
     } catch (error) {
-      Console.log("error on page "+index);
-      Console.log(error);
+      logHelper("error on page " + index);
+      logHelper(error);
     }
   });
 
@@ -87,35 +85,43 @@ function run(input, params) {
     'facet': "DataLossPrevention",
     'save': false
   });
-  
+
   input["dlp:sensitiveData"] = true;
   input["dlp:scanStatus"] = "done";
 
   //save findings
   input = Document.AddItemToListProperty(input, {
-    /*required:true - type: string*/
     'complexJsonProperties': JSON.stringify(findings),
-    /*required:true - type: string*/
     'xpath': "dlp:findings",
-    /*required:false - type: boolean*/
     'save': true
   });
 
-  Console.log("Add annotations...");
+  logHelper("Add annotations...");
 
   //creation redaction annotations
   input = NEV.CreateDlpAnnotations(input, {});
-  
-  Console.log("\n==============================\nTestDLP.......DONE\n==============================");
-  
-}
 
+  logHelper("================================================================================")
+  logHelper("== Done.");
+  logHelper("================================================================================")
+}
 
 function toJsArray(javaArray) {
   var jsArray = [];
-  for(var i=0; javaArray && (i < javaArray.length ); i++) {
+  for (var i = 0; javaArray && (i < javaArray.length); i++) {
     jsArray.push(javaArray[i]);
   }
   return jsArray;
+}
+
+//==============================================================================
+// Prefix for log messages to make it easier to understand the context.
+var LOG_PREFIX = "api_DLP_Process: ";
+// Turn log messages on or off globally.
+var LOGGING_ENABLED = true;
+
+function logHelper(message) {
+  if (LOGGING_ENABLED)
+    Console.warn(LOG_PREFIX + message);
 }
 
