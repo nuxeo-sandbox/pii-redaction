9a1b4fcacfe17dfe9e1a1d74497c10b02c748db9
jfletcher <josh.fletcher@hyland.com>
2026-01-28 18:57:30 +0000

    Update Automation Scripting/piir_api_DLP_CleanupData

--- studio/.metadata/scripts/piir_api_DLP_CleanupData.scriptedOperation.js
+++ studio/.metadata/scripts/piir_api_DLP_CleanupData.scriptedOperation.js
@@ -25,7 +25,6 @@ function run(input, params) {
 
 }
 
-
 //==============================================================================
 // Prefix for log messages to make it easier to understand the context.
 var LOG_PREFIX = "piir_api_DLP_CleanupData: ";
